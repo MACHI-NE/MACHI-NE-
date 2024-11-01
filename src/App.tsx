a8f934a3aaf3da0359c6b1e02f9bc14b4ca10e01
Coleman Lai <colemancflai@yahoo.com>
2024-10-31 22:24:09 -0700

    Added required dependencies Leaflet and MD5

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center p-4">
+        MACHI-NE-
+      </h1>
     </div>
   );
 }
